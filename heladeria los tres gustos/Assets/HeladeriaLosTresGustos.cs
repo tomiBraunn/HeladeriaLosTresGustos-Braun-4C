330eb43a8f865b75901194d441d07a7ebe912fd9
Tomas B <tomasbraun2008@gmail.com>
2025-04-10 18:06:07 -0300

    funca

---
+++ heladeria los tres gustos/Assets/HeladeriaLosTresGustos.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeladeriaLosTresGustos : MonoBehaviour
+{
+    public string gustoDeHelado;
+    public int cantidadDeHelado;
+
+    private float precioPorKilo = 500f;
+    private float costoFinal;
+
+    void Start()
+    {
+        if ((gustoDeHelado != "CHO" && gustoDeHelado != "FRU" && gustoDeHelado != "DDL") || 
+            (cantidadDeHelado < 250 || cantidadDeHelado > 3000))
+        {
+            Debug.Log("Por favor ingresa un gusto válido o una cantidad válida.");
+            return;
+        }
+
+        float precioFinalPorKilo = precioPorKilo;
+
+        if (gustoDeHelado == "FRU")
+        {
+            precioFinalPorKilo *= 0.9f; 
+        }
+
+        costoFinal = (cantidadDeHelado / 1000f) * precioFinalPorKilo;
+
+        Debug.Log("El costo del pedido es: $" + costoFinal);
+    }
+
+    void Update()
+    {
+
+    }
+}
